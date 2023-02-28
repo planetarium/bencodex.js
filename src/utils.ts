d087a60cab47967c9b1252050c23f66674697bb5
Hong Minhee <hong@minhee.org>
2023-02-28 23:42:33 +0900

    Natural number decoder

--- src/utils.ts
+++ src/utils.ts
@@ -30,3 +30,79 @@ export function compareUint8Arrays(a: Uint8Array, b: Uint8Array): number {
   if (a.length > b.length) return 1;
   return 0;
 }
+
+/**
+ * Represents the end state of {@link decodeAsciiNaturalNumber}.
+ * @typeParam The type of the decoded value.
+ */
+export type NaturalNumberDecodingState<T extends number | bigint> =
+  /** The case where decoding a natural number succeeds. */
+  | {
+    /** Whether it is successful.  It's always `true` in this case. */
+    success: true;
+    /** The number of read bytes.  It's always greater than 0 in this case. */
+    read: number;
+    /** The decoded number. */
+    value: T;
+  }
+  /** The case where decoding a natural number fails. */
+  | {
+    /** Whether it is successful.  It's always `false` in this case. */
+    success: false;
+    /** The number of read bytes.  It's always zero in this case. */
+    read: 0;
+  };
+
+export function decodeAsciiNaturalNumber(
+  buffer: Uint8Array,
+  type: "number",
+): NaturalNumberDecodingState<number>;
+
+export function decodeAsciiNaturalNumber(
+  buffer: Uint8Array,
+  type: "bigint",
+): NaturalNumberDecodingState<bigint>;
+
+/**
+ * Decodes ASCII-encoded digits in the given buffer into a natural number.
+ * It does not parse signed numbers.
+ * @typeParam The type of the decoded value.
+ * @param type The type to represent the decoded value.
+ * @param buffer A buffer that starts with ASCII-encoded digits to decode.
+ * @returns An object representing the end-state.  Its `success` field contains
+ *          a Boolean value that shows if it succeeded. Its `read` field
+ *          contains the number of read bytes.  If it was successful,
+ *          it contains one more field named `value` which contains the parsed
+ *          number.
+ */
+export function decodeAsciiNaturalNumber(
+  buffer: Uint8Array,
+  type: "number" | "bigint",
+): NaturalNumberDecodingState<number | bigint> {
+  let read = 0;
+  if (type === "bigint") {
+    let value = 0n;
+    while (
+      read < buffer.length && 0x30 <= buffer[read] && buffer[read] <= 0x39
+    ) {
+      value *= 10n;
+      const digit = BigInt(buffer[read]) - 0x30n;
+      value += digit;
+      read++;
+    }
+    if (read < 1) return { success: false, read: 0 };
+    return { success: true, read, value };
+  } else {
+    let value = 0;
+    while (
+      read < buffer.length && 0x30 <= buffer[read] && buffer[read] <= 0x39
+    ) {
+      value *= 10;
+      const digit = buffer[read] - 0x30;
+      value += digit;
+      read++;
+    }
+    if (read < 1) return { success: false, read: 0 };
+    return { success: true, read, value };
+  }
+}
--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,9 +1,14 @@
 import {
   assert,
+  assertEquals,
   assertFalse,
   assertStrictEquals,
 } from "std/testing/asserts.ts";
-import { areUint8ArraysEqual, compareUint8Arrays } from "../src/utils.ts";
+import {
+  areUint8ArraysEqual,
+  compareUint8Arrays,
+  decodeAsciiNaturalNumber,
+} from "../src/utils.ts";
 
 Deno.test("areUint8ArraysEqual()", () => {
   const a = new Uint8Array([0x01, 0x02, 0x03]);
@@ -25,4 +30,46 @@ Deno.test("compareUint8Arrays()", () => {
   assert(compareUint8Arrays(c, a) > 0);
   assert(compareUint8Arrays(a, d) < 0);
   assert(compareUint8Arrays(d, a) > 0);
+});
+
+Deno.test("decodeAsciiNaturalNumber()", async (t: Deno.TestContext) => {
+  await t.step("number", () => {
+    const buffer = new Uint8Array(128);
+    const textEncoder = new TextEncoder();
+    textEncoder.encodeInto("1234", buffer);
+    assertEquals(
+      decodeAsciiNaturalNumber(buffer, "number"),
+      { success: true, read: 4, value: 1234 },
+    );
+    buffer[2] = 0x3a;
+    assertEquals(
+      decodeAsciiNaturalNumber(buffer, "number"),
+      { success: true, read: 2, value: 12 },
+    );
+    buffer[0] = 0xff;
+    assertEquals(decodeAsciiNaturalNumber(buffer, "number"), {
+      success: false,
+      read: 0,
+    });
+  });
+
+  await t.step("bigint", () => {
+    const buffer = new Uint8Array(128);
+    const textEncoder = new TextEncoder();
+    textEncoder.encodeInto("1234", buffer);
+    assertEquals(
+      decodeAsciiNaturalNumber(buffer, "bigint"),
+      { success: true, read: 4, value: 1234n },
+    );
+    buffer[2] = 0x3a;
+    assertEquals(
+      decodeAsciiNaturalNumber(buffer, "bigint"),
+      { success: true, read: 2, value: 12n },
+    );
+    buffer[0] = 0xff;
+    assertEquals(decodeAsciiNaturalNumber(buffer, "bigint"), {
+      success: false,
+      read: 0,
+    });
+  });
 });
